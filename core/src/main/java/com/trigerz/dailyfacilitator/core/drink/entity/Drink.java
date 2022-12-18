9753a95e61b86b28724d491a2cab9cc3c0e71099
Franck Mazzolo <franck.mazzolo@gmail.com>
2022-12-18 07:30:59 +0100

    Drink entity

--- core/src/main/java/com/trigerz/dailyfacilitator/core/drink/entity/Drink.java
+++ core/src/main/java/com/trigerz/dailyfacilitator/core/drink/entity/Drink.java
@@ -2,11 +2,13 @@ package com.trigerz.dailyfacilitator.core.drink.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Drink implements Serializable {
 
