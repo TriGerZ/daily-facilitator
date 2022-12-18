f598515453261f64b19a7d3c861755f7f0603df6
Franck Mazzolo <franck.mazzolo@gmail.com>
2022-12-18 07:32:18 +0100

    Rest entrypoints

--- infrastructure/src/main/java/com/trigerz/dailyfacilitator/infrastructure/drink/entrypoints/rest/DrinkApiPost.java
+++ infrastructure/src/main/java/com/trigerz/dailyfacilitator/infrastructure/drink/entrypoints/rest/DrinkApiPost.java
@@ -1,21 +1,14 @@
 package com.trigerz.dailyfacilitator.infrastructure.drink.entrypoints.rest;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class DrinkApi {
-
-	@Id
-	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	private Long id;
+public class DrinkApiPost {
 	private String name;
 }
--- infrastructure/src/main/java/com/trigerz/dailyfacilitator/infrastructure/drink/entrypoints/rest/DrinkEndpoint.java
+++ infrastructure/src/main/java/com/trigerz/dailyfacilitator/infrastructure/drink/entrypoints/rest/DrinkEndpoint.java
@@ -3,10 +3,7 @@ package com.trigerz.dailyfacilitator.infrastructure.drink.entrypoints.rest;
 import com.trigerz.dailyfacilitator.core.drink.usecase.CreateDrink;
 import com.trigerz.dailyfacilitator.core.drink.usecase.GetAllDrinks;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -19,6 +16,7 @@ public class DrinkEndpoint {
 	private final CreateDrink createDrink;
 	private final GetAllDrinks getAllDrinks;
 	private final DrinkApiMapper drinkApiMapper;
+	private final DrinkApiPostMapper drinkApiPostMapper;
 
 	@GetMapping
 	public Collection<DrinkApi> getAllDrinks(){
@@ -26,7 +24,7 @@ public class DrinkEndpoint {
 	}
 
 	@PostMapping
-	public void createDrink(DrinkApi drinkApi){
-		this.createDrink.execute(drinkApiMapper.toDrink(drinkApi));
+	public void createDrink(@RequestBody DrinkApiPost drinkApiPost){
+		this.createDrink.execute(drinkApiPostMapper.toDrink(drinkApiPost));
 	}
 }
